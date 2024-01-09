fbbb16a5ed6a2ab1d232d97d09e8778f52d5d450
Rubby <rubenslombard@pursuit.org>
2024-01-09 10:31:38 -0500

    navbar and css added

--- frutier/src/Components/NavBar.js
+++ frutier/src/Components/NavBar.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import { FaBell, FaBars, FaTimes } from "react-icons/fa";
+import { useRef } from 'react';
 
 export default function NavBar() {
+    const navBarRef = useRef();
+
+    const activatedNavBar = () => {
+        navBarRef.current.classlist.toggle("responsive-navBar")
+    }
+
   return (
-      <div className='Nav'>
-          <h1>Fruitier</h1>
+    <div className="Nav">
+      <a className="logo" href="/#">
+        FRUITIER
+      </a>
+      <nav className="navbar">
+        <a href="/#"> Home </a>
+        <a href="/#"> Fruits </a>
+        <a href="/#"> about </a>
+        <button>
+          {" "}
+          <FaBell />{" "}
+        </button>{" "}
+        <button nav-close-btn onClick={activatedNavBar}>
+          <FaTimes />
+        </button>
+      </nav>
+      <button className="nav-btn" onClick={activatedNavBar}>
+        <FaBars />
+      </button>
     </div>
-  )
+  );
 }
